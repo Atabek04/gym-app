3c3201ebbc14a8eda8a943b9c9534827672baeb6
Atabek-dev <220320@astanait.edu.kz>
2024-12-21 00:50:27 +0500

    create parent pom.xml

--- gym-app/src/main/resources/db/migration/V13__create_training_type.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE training_type
-(
-    id                 SERIAL PRIMARY KEY,
-    training_type_name VARCHAR(50) NOT NULL
-);
--- gym-app/src/main/resources/db/migration/V14__create_trainer.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE trainer
-(
-    id               SERIAL PRIMARY KEY,
-    user_id          BIGINT  NOT NULL,
-    training_type_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users_table (id)
-);
