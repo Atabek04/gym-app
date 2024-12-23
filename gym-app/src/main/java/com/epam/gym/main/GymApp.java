248e152f114ca64f5cf71c4a2f268a4333f62d5e
Atabek-dev <220320@astanait.edu.kz>
2024-12-23 10:31:50 +0500

    fix dependency conflicts

--- gym-app/src/main/java/com/epam/gym/main/GymApp.java
+++ gym-app/src/main/java/com/epam/gym/main/GymApp.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
@@ -18,7 +17,6 @@ import java.util.Map;
 
 @SpringBootApplication(scanBasePackages = "com.epam.gym")
 @EnableDiscoveryClient
-@EnableFeignClients
 public class GymApp {
     public static void main(String[] args) {
         SpringApplication.run(GymApp.class, args);
